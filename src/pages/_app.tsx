468853ad2f7abd25cbd365e650502eb4a79764eb
Anto Subash <antosubash@live.com>
2020-09-19 13:51:20 +0200

    added highlight js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import "../node_modules/highlight.js/styles/dark.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ const Index = ({ allPosts }: Props) => {
     <>
       <Layout>
         <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
+          <title>Anto's blog</title>
         </Head>
         <Container>
           <Intro />
