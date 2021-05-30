316729a6c517b352bd366cb7ec4ebed3e82b235b
Anto Subash <antosubash@live.com>
2021-05-30 14:32:23 +0200

    updated text size

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,16 +1,16 @@
-import { CMS_NAME } from '../lib/constants'
+import { CMS_NAME } from "../lib/constants";
 
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="text-3xl md:text-5xl font-bold tracking-tighter leading-tight md:pr-8">
         Anto Subash.
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A personal blog from a lazy programmer.
       </h4>
     </section>
-  )
-}
+  );
+};
 
-export default Intro
+export default Intro;
