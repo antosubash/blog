c381ea2d7d576dcaf5ae53ff4ebccd8033c86b04
Anto Subash <antosubash@live.com>
2021-07-27 00:41:03 +0200

    Update markdownToHtml.ts

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,7 @@
-import remark from 'remark'
-import html from 'remark-html'
-const highlight = require('remark-highlight.js');
+import remark from "remark";
+import html from "remark-html";
+const highlight = require("remark-highlight.js");
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(highlight).use(html).process(markdown)
-  return result.toString()
+  const result = await remark().use(highlight).use(html).process(markdown);
+  return result.toString();
 }
