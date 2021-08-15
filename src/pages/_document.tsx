37c2d710cc110bcf41248e9a80453a8a351a9aee
Anto Subash <antosubash@live.com>
2021-08-15 17:53:19 +0200

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,7 +25,7 @@ export default class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
         </Head>
-        <body>
+        <body className="bg-white dark:bg-black text-gray-900 dark:text-gray-100">
           <Main />
           <NextScript />
         </body>
