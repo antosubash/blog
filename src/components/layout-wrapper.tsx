8f25008af49c6aa1c73742c967394e41068d50f3
Anto Subash <antosubash@live.com>
2021-12-11 22:40:44 +0100

    fix for mobile

--- src/components/layout-wrapper.tsx
+++ src/components/layout-wrapper.tsx
@@ -16,10 +16,37 @@ const LayoutWrapper = ({ children }: any) => {
           </div>
 
           <div className="flex items-center text-base leading-5">
-            <a href="/" className="font-bold text-xl px-5" >Home</a>
-            <a href="/tags" className="font-bold text-xl px-5" >Tags</a>
-            <a href="https://antosubash.com" className="font-bold text-xl px-5" >About</a>
+            <div className="hidden md:block">
+              <a href="/" className="font-bold px-5">
+                Home
+              </a>
+              <a href="/tags" className="font-bold px-5">
+                Tags
+              </a>
+              <a href="https://antosubash.com" className="font-bold px-5">
+                About
+              </a>
+            </div>
+
             <ThemeSwitch />
+            <div className="hidden">
+              <button className="w-8 h-8 p-1 ml-1 mr-1 rounded sm:ml-4">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
         </header>
         <main className="mb-auto">{children}</main>
--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import kebabCase  from "../lib/utils/kebabCase";
+import kebabCase from "../lib/utils/kebabCase";
 
 const Tag = ({ text }: any) => {
   return (
     <Link href={`/tags/${text}`}>
-      <a className="text-sm mx-2 inline-flex items-center justify-center font-bold leading-sm uppercase px-3 py-1 bg-gray-400 dark:bg-gray-600 rounded-full">
+      <a className="text-sm m-2 inline-flex items-center justify-center font-bold leading-sm uppercase px-3 py-1 bg-gray-400 dark:bg-gray-600 rounded-full">
         {text}
       </a>
     </Link>
