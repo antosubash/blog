f70c59d077c115a576208fce72522c7742908e0a
Anto Subash <antosubash@live.com>
2022-01-10 22:52:27 +0100

    site map generated.

--- src/_posts/abp/abp-add-tailwindcss-to-dotnet-mvc.md
+++ src/_posts/abp/abp-add-tailwindcss-to-dotnet-mvc.md
@@ -1,7 +1,7 @@
 ---
 title: "Add tailwindcss to your ABP MVC UI"
 excerpt: "In this post we will see how to add tailwind css to your ABP MVC UI."
-date: "2022-01-05"
+date: "2022-01-11"
 videoId:
 tags: [ "abp", "dotnet", "tailwind" ]
 author:
---
+++ src/scripts/generate-sitemap.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const globby = require('globby')
+const prettier = require('prettier')
+const path = require('path')
+
+; (async () => {
+
+    const pages = await globby([
+        'pages/*.tsx',
+        '_posts/**/*.md',
+        '!pages/_*.tsx',
+        '!pages/api',
+    ])
+    var parse = path.parse;
+    const sitemap = `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${pages
+            .map((page) => {
+                //var fileName = parse(page).name;
+                //var url = `https://blog.antosubash.com/posts/${fileName}`;
+                var fileName = parse(page).name;
+                var route = fileName === 'index' ? '' : `/posts/${fileName}`;
+                console.log(route);
+                return `
+                        <url>
+                            <loc>https://blog.antosubash.com${route}</loc>
+                        </url>
+                    `
+            })
+            .join('')}
+        </urlset>
+    `
+
+    const formatted = prettier.format(sitemap, {
+        parser: 'html',
+    })
+
+    // eslint-disable-next-line no-sync
+    fs.writeFileSync('public/sitemap.xml', formatted)
+})()
