e41c020959a58adb245cbd478527b01b44f7ba1b
Anto Subash <antosubash@live.com>
2022-03-31 10:01:21 +0200

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const HOME_OG_IMAGE_URL = "https://blog.antosubash.com/assets/blog/authors/anto.jpg";
-export const POSTS_PER_PAGE = 5;
-export const MAX_DISPLAY = 5
+export const POSTS_PER_PAGE = 10;
+export const MAX_DISPLAY = 5;
 export const SITE_URI = 'https://blog.antosubash.com/';
 export const HOSTNAME = 'blog.antosubash.com';
