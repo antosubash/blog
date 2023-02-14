830a72f784421d374652fa6358e1223fa05ff69f
Anto Subash <antosubash@live.com>
2023-02-14 23:55:57 +0100

    removed unused things

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -1,31 +1,7 @@
 import { Code } from "./code";
 import { Pre } from "./pre";
 
-const ExternalLink = ({ href, children }: any) => {
-  const siteUrl = "";
-  const isRoot = href[0] === "/";
-  const isInternalLink = href.startsWith(siteUrl) || isRoot;
-
-  if (isInternalLink) {
-    return <a href={href}>{children}</a>;
-  }
-
-  return (
-    <a
-      className="bg-blue-800"
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      {children}
-    </a>
-  );
-};
-
-
-
 const MDXComponents = {
-  a: ExternalLink,
   code: Code,
   pre: Pre
 };
