f1108219b0c0e6193a6650d09df3885e3b0d29c7
Anto Subash <antosubash@live.com>
2023-07-23 17:00:34 +0200

    Create convert-array-to-list.js

---
+++ src/scripts/convert-array-to-list.js
@@ -0,0 +1,41 @@
+console.log('convert-array-to-list.js');
+var path = require('path');
+var fs = require('fs');
+const postsDirectory = path.join(process.cwd(), "_posts");
+
+const getAllFiles = function (dirPath, arrayOfFiles) {
+    files = fs.readdirSync(dirPath)
+
+    arrayOfFiles = arrayOfFiles || []
+
+    files.forEach(function (file) {
+        if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+            arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles)
+        } else {
+            arrayOfFiles.push(path.join(__dirname, dirPath, "/", file))
+        }
+    })
+
+    return arrayOfFiles
+}
+const fileNames = getAllFiles(postsDirectory);
+
+console.log(fileNames);
+
+for (let i = 0; i < fileNames.length; i++) {
+    // convert the tags array to a list of tags in the frontmatter
+    const file = fileNames[i];
+    const fileContents = fs.readFileSync(file, 'utf8');
+    const lines = fileContents.split('\n');
+    const tagsIndex = lines.findIndex(line => line.startsWith('tags:'));
+    if (tagsIndex > -1) {
+        // convert the tags array to a list of tags in the frontmatter
+        const tagsLine = lines[tagsIndex];
+        const tags = tagsLine.split(':')[1].trim();
+        const tagsList = tags.split(',').map(tag => tag.trim());
+        const tagsListString = tagsList.map(tag => `- ${tag}`).join('\n');
+        lines[tagsIndex] = `tags:\n${tagsListString}`;
+        const newFileContents = lines.join('\n');
+        fs.writeFileSync(file, newFileContents);
+    }
+}   
