629a51e374e2db14d8087999c25eb426289dea62
Anto Subash <antosubash@live.com>
2024-03-20 23:56:40 +0100

    Add LD_LIBRARY_PATH configuration for canvas module

--- v2/next.config.js
+++ v2/next.config.js
@@ -1,3 +1,13 @@
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${process.env.PWD
+    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
 const { withContentlayer } = require('next-contentlayer')
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
