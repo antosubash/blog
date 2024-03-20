818da61b6cce8c36e79234b31d873dd755fc28d1
Anto Subash <antosubash@live.com>
2024-03-21 00:05:42 +0100

    Remove unused code and update postbuild script

--- v2/scripts/postbuild.mjs
+++ v2/scripts/postbuild.mjs
@@ -1,11 +1,9 @@
-import generateAllOgImagesIfNeeded from './og.mjs'
 import rss from './rss.mjs'
 import searchIndex from './search.mjs'
 
 async function postbuild() {
   await rss()
   await searchIndex()
-  await generateAllOgImagesIfNeeded()
 }
 
 postbuild()
