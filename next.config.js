35f817fa0748bdf5e716086b53f8adec35b28862
Anto Subash <antosubash@live.com>
2025-01-21 13:25:02 +0100

    Enhance .NET OpenAPI tutorial with sample API documentation image and update next.config.js to allow image loading from raw.githubusercontent.com

--- next.config.js
+++ next.config.js
@@ -72,6 +72,10 @@ module.exports = () => {
           protocol: 'https',
           hostname: 'picsum.photos',
         },
+        {
+          protocol: 'https',
+          hostname: 'raw.githubusercontent.com',
+        },
       ],
     },
     async headers() {
